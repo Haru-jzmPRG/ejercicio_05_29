ec8b521e1889e2b1d5dbb16ee2ed3c924504e7e8
Jazmin <programajazminc20@gmail.com>
2025-10-28 14:14:45 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int n1 = Integer.parseInt(System.console().readLine("Introduzca un número positivo (relativamente grande): "));
+        int n2 = Integer.parseInt(System.console().readLine("Introduzca otro número (relativamente pequeño): "));
+
+        System.out.println("Los números positivos menores que " + n1 + " que no son divisibles entre " + n2 + " son los siguientes: ");
+
+        for (int i = 1; i < n1; i++) {
+            if (i % n2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
